5e87143e55ffc67f55ddcdaddee4ca1f28b1b473
SandipNayak <46400343+SandipNayak@users.noreply.github.com>
2019-01-06 19:21:01 +0530

    Updated

--- q6.java
+++ q6.java
@@ -1,21 +1,16 @@
-package codex;
+pacakge codex;
 
-public class q6 {
+
+public class q_6_ {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int i,j,prod=0;
-int sum=0;
-int sum1=0;
-for(i=1;i<=100;i++)
-{
-	sum=sum+i*i;
-	sum1=sum1+i;
-}
-prod=sum1*sum1;
-int diff;
-diff=prod-sum;
-System.out.println("The difference is"+diff);
+int n=100;
+int sum=(n*(n+1))/2;
+int a=sum*sum;
+int sumsq=(n*(n+1)*(2*n+1))/6;
+int diff=a-sumsq;
+System.out.println("difference="+diff);
 	}
 
 }
